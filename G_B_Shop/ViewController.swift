2a3875f36e28ce3d44dceb48ccfcffe5d14b1d41
Emil Kurbanov <kurbanov_emil@icloud.com>
2022-01-03 15:22:55 +0300

    Feature/HW1.1

---
+++ G_B_Shop/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  G_B_Shop
+//
+//  Created by emil kurbanov on 03.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
